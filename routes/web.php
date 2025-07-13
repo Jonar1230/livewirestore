2bb77e57c115b21219d4107f717746ad48994f7c
Jonar1230 <jramirez0490@gmail.com>
2025-07-12 20:28:15 -0400

    rutas

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,36 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+
+  Route::group([
+        'prefix' => 'dashboard'
+    ], function () {
+
+        Route::get('/', function () {
+            return view('dashboard');
+        })->name('dashboard');
+
+        //category
+        Route::group(['prefix' => 'category'], function () {
+            Route::get('/', App\Livewire\Dashboard\Category\Index::class)->name("d-category-index");
+            Route::get('/create', App\Livewire\Dashboard\Category\Save::class)->name("d-category-create");
+            Route::get('/edit/{id}', App\Livewire\Dashboard\Category\Save::class)->name("d-category-edit");
+        });
+
+        //post
+        Route::group(['prefix' => 'post'], function () {
+            Route::get('/', App\Livewire\Dashboard\Post\Index::class)->name("d-post-index");
+            Route::get('/create', App\Livewire\Dashboard\Post\Save::class)->name("d-post-create");
+            Route::get('/edit/{id}', App\Livewire\Dashboard\Post\Save::class)->name("d-post-edit");
+        });
+
+        //tag
+        Route::group(['prefix' => 'tag'], function () {
+            Route::get('/', App\Livewire\Dashboard\Tag\Index::class)->name("d-tag-index");
+            Route::get('/create', App\Livewire\Dashboard\Tag\Save::class)->name("d-tag-create");
+            Route::get('/edit/{id}', App\Livewire\Dashboard\Tag\Save::class)->name("d-tag-edit");
+        });
+
+    });
