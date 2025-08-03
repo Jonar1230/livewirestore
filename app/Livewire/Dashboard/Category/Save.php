3866f77e68237ac2241aadf5d3c1b64815237be3
Jonathan Ramírez <jramirez0490@gmail.com>
2025-08-02 21:12:17 -0400

    avance categoria

--- app/Livewire/Dashboard/Category/Save.php
+++ app/Livewire/Dashboard/Category/Save.php
@@ -2,12 +2,32 @@
 
 namespace App\Livewire\Dashboard\Category;
 
+use App\Models\Category;
+
 use Livewire\Component;
+
 
 class Save extends Component
 {
+
+    public $title;
+    public $slug;
+    public $text;
+
     public function render()
     {
         return view('livewire.dashboard.category.save');
     }
+
+    public function submit()
+    {
+       
+        Category::create([
+            'title' => $this->title,
+            'slug' => str($this->title)->slug(),
+            'text' => $this->text,
+        ]);
+
+    }
+
 }
--- resources/views/livewire/dashboard/category/save.blade.php
+++ resources/views/livewire/dashboard/category/save.blade.php
@@ -1,3 +1,9 @@
 <div>
-    {{-- The whole world belongs to you. --}}
+
+  <form wire:submit.prevent="submit">
+    <input type="text" wire:model="title" placeholder="Title">
+    <textarea wire:model="text" placeholder="Text"></textarea>
+    <button type="submit" wire:click="submit">Send</button>
+  </form>
+
 </div>
