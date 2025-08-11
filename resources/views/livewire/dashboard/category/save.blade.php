1e134f13c809b77bc5cefb4119b7bbe5584f0c3c
Jonathan Ramírez <jramirez0490@gmail.com>
2025-08-10 20:48:50 -0400

    uso de flux

--- resources/views/livewire/dashboard/category/save.blade.php
+++ resources/views/livewire/dashboard/category/save.blade.php
@@ -1,9 +1,16 @@
 <div>
 
+<link href="{{ asset('app.css') }}" rel="stylesheet" />
+
+
   <form wire:submit.prevent="submit">
-    <input type="text" wire:model="title" placeholder="Title">
-    <textarea wire:model="text" placeholder="Text"></textarea>
-    <button type="submit" wire:click="submit">Send</button>
+   
+    <flux:input type="text" wire:model="title" placeholder="Title" :label="__('Title')" />
+
+    <flux:textarea wire:model="text" placeholder="Text" :label="__('Text')" />
+
+    <flux:button type="button" wire:click="submit">{{ __('Save') }}</flux:button>
+
   </form>
 
 </div>
